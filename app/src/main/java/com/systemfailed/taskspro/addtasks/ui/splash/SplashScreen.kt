c2a70b42ed275a04541a022bd4645f7fbef592bb
Jose Carlos Joao Moreno Aleman <joao.moreno.aleman@gmail.com>
2023-12-16 11:07:04 -0500

    splashscreen creation

---
+++ app/src/main/java/com/systemfailed/taskspro/addtasks/ui/splash/SplashScreen.kt
@@ -0,0 +1,73 @@
+package com.systemfailed.taskspro.addtasks.ui.splash
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AddTask
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.systemfailed.taskspro.ui.theme.newFamily
+
+@Composable
+fun SplashScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxSize()
+            .background(color = Color.DarkGray),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Icon(
+            imageVector = Icons.Filled.AddTask,
+            contentDescription = "",
+            tint = Color.Yellow,
+            modifier = Modifier
+                .size(72.dp)
+        )
+        Text(
+            buildAnnotatedString {
+                withStyle(
+                    style = SpanStyle(
+                        color = Color.White,
+                        textDecoration = TextDecoration.Underline
+                    )
+                ) {
+                    append("Tasks")
+                }
+                append("Pro")
+            },
+            fontSize = 40.sp,
+            fontFamily = newFamily,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier
+                .padding(16.dp)
+        )
+    }
+}
+
+@Preview(
+    showBackground = true,
+    showSystemUi = true
+)
+@Composable
+fun SplashScreenPreview() {
+    SplashScreen()
+}
